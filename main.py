2c9f3ea4cfaec5cf143f755cf9c79dc3c6a6d884
Brendan Kerr <brendankerr@Brendans-MacBook-Pro.local>
2021-11-29 18:45:09 -0500

    Add main method to execute etl process and complete assignment

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 """
-This is the entrypoint to the program. 'python main.py' will be executed and the 
+This is the entrypoint to the program. 'python main.py' will be executed and the
 expected csv file should exist in ../data/destination/ after the execution is complete.
 """
+import os
+import pathlib
+import src.etl_utils as etl
+from src.some_storage_library import SomeStorageLibrary
 
+project_root = pathlib.Path(__file__).parent.parent.resolve()
+column_file = os.path.join(project_root, 'data', 'source', 'SOURCECOLUMNS.txt')
+data_file = os.path.join(project_root, 'data', 'source', 'SOURCEDATA.txt')
+output_file = 'output.csv'
+
+
+def main():
+    etl.create_csv_from_raw_data(column_file, data_file, output_file)
+    storage = SomeStorageLibrary()
+    storage.load_csv(output_file)
 
 
 if __name__ == '__main__':
     """Entrypoint"""
     print('Beginning the ETL process...')
+    main()
